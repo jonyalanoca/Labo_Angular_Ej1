1a1b3136721063465cf7956fbb1c2cf64319a25a
jonyalanoca <jonyalanoca2@gmail.com>
2023-09-04 02:00:09 -0300

    Parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Clase1Ejercicios';
+  public edad1?:number;
+  public edad2?:number;
+  public promedioEdades?:number;
+  public sumaEdades?:number;
+  public Calcular(event?: any):void{
+    if(this.edad1!=undefined && this.edad2!=undefined){
+      this.sumaEdades=parseInt(this.edad1.toString())+parseInt(this.edad2.toString());
+      this.promedioEdades=this.sumaEdades/2;
+    }
+  }
+  public Limpiar(event?: any):void{
+    this.edad1=undefined;
+    this.edad2=undefined;
+    this.promedioEdades=undefined;
+    this.sumaEdades=undefined;
+  }
 }
